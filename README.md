393ff9caab58d849668d41bae5b97f315e0a20ee
phil-bell <philhabell@gmail.com>
2021-02-01 21:11:21 +0000

    Adding help text and better readme

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # `s3-loader`
 
+**Install**
+```console
+pip install s3-loader
+```
+
 **Usage**:
 
 ```console
@@ -14,23 +19,25 @@ $ s3-loader [OPTIONS] COMMAND [ARGS]...
 
 **Commands**:
 
-* `auth`
-* `clear-bucket`
-* `upload-dir`
-* `upload-file`
+* `auth`: authenticate with your aws access key and...
+* `clear-bucket`: delete everything in an s3 bucket
+* `upload-dir`: upload an entire directory to s3
+* `upload-file`: upload a single file to s3
 
 ## `s3-loader auth`
+
+authenticate with your aws access key and secret
 
 **Usage**:
 
 ```console
-$ s3-loader auth [OPTIONS] AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+$ s3-loader auth [OPTIONS] [AWS_ACCESS_KEY_ID] [AWS_SECRET_ACCESS_KEY]
 ```
 
 **Arguments**:
 
-* `AWS_ACCESS_KEY_ID`: [required]
-* `AWS_SECRET_ACCESS_KEY`: [required]
+* `[AWS_ACCESS_KEY_ID]`: Your aws access key  [default: ]
+* `[AWS_SECRET_ACCESS_KEY]`: Your aws secret acess ket  [default: ]
 
 **Options**:
 
@@ -38,15 +45,17 @@ $ s3-loader auth [OPTIONS] AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 
 ## `s3-loader clear-bucket`
 
+delete everything in an s3 bucket
+
 **Usage**:
 
 ```console
-$ s3-loader clear-bucket [OPTIONS] BUCKETNAME
+$ s3-loader clear-bucket [OPTIONS] [BUCKET_NAME]
 ```
 
 **Arguments**:
 
-* `BUCKETNAME`: [required]
+* `[BUCKET_NAME]`: Your bucket name  [default: ]
 
 **Options**:
 
@@ -54,16 +63,18 @@ $ s3-loader clear-bucket [OPTIONS] BUCKETNAME
 
 ## `s3-loader upload-dir`
 
+upload an entire directory to s3
+
 **Usage**:
 
 ```console
-$ s3-loader upload-dir [OPTIONS] PATH BUCKETNAME
+$ s3-loader upload-dir [OPTIONS] [PATH] [BUCKET_NAME]
 ```
 
 **Arguments**:
 
-* `PATH`: [required]
-* `BUCKETNAME`: [required]
+* `[PATH]`: Path to your directory  [default: ]
+* `[BUCKET_NAME]`: Your bucket name  [default: ]
 
 **Options**:
 
@@ -71,17 +82,19 @@ $ s3-loader upload-dir [OPTIONS] PATH BUCKETNAME
 
 ## `s3-loader upload-file`
 
+upload a single file to s3
+
 **Usage**:
 
 ```console
-$ s3-loader upload-file [OPTIONS] LOCAL_FILE BUCKET S3_FILE
+$ s3-loader upload-file [OPTIONS] [LOCAL_FILE] [BUCKET] [S3_FILE]
 ```
 
 **Arguments**:
 
-* `LOCAL_FILE`: [required]
-* `BUCKET`: [required]
-* `S3_FILE`: [required]
+* `[LOCAL_FILE]`: Local file name  [default: ]
+* `[BUCKET]`: Your bucket name  [default: ]
+* `[S3_FILE]`: Name the file will use on s3  [default: ]
 
 **Options**:
 
--- s3_loader/main.py
+++ s3_loader/main.py
@@ -1,17 +1,26 @@
 import os
-import boto3
 import typer
+import boto3
 from botocore.exceptions import NoCredentialsError
 
 app = typer.Typer()
 
-@app.command()
-def auth(aws_access_key_id: str, aws_secret_access_key: str):
+
+@app.command(help="authenticate with your aws access key and secret")
+def auth(
+    aws_access_key_id: str = typer.Argument("", help="Your aws access key"),
+    aws_secret_access_key: str = typer.Argument("", help="Your aws secret acess ket"),
+):
     os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key_id
     os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key
 
-@app.command()
-def upload_file(local_file: str, bucket: str, s3_file: str):
+
+@app.command(help="upload a single file to s3")
+def upload_file(
+    local_file: str = typer.Argument("", help="Local file name"),
+    bucket: str = typer.Argument("", help="Your bucket name"),
+    s3_file: str = typer.Argument("", help="Name the file will use on s3"),
+):
     s3 = boto3.client(
         "s3",
         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
@@ -20,23 +29,30 @@ def upload_file(local_file: str, bucket: str, s3_file: str):
     try:
         s3.upload_file(local_file, bucket, s3_file)
     except FileNotFoundError:
-        print("file not found")
+        print("ERROR: file not found")
     except NoCredentialsError:
-        print("no credentials")
+        print("ERROR: no credentials")
 
-@app.command()
-def upload_dir(path: str, bucketname: str):
+
+@app.command(help="upload an entire directory to s3")
+def upload_dir(
+    path: str = typer.Argument("", help="Path to your directory"),
+    bucket_name: str = typer.Argument("", help="Your bucket name"),
+):
     for root, dirs, files in os.walk(path):
         for file in files:
-            upload_file(os.path.join(root, file), bucketname, file)
+            upload_file(os.path.join(root, file), bucket_name, file)
 
 
-@app.command()
-def clear_bucket(bucketname: str):
+@app.command(help="delete everything in an s3 bucket")
+def clear_bucket(bucket_name: str = typer.Argument("", help="Your bucket name")):
     s3 = boto3.resource("s3")
-    bucket = s3.Bucket(bucketname)
+    bucket = s3.Bucket(bucket_name)
     bucket.objects.all().delete()
 
 
 if __name__ == "__main__":
-    app()
+    if "AWS_ACCESS_KEY_ID" in os.environ and "AWS_SECRET_ACCESS_KEY" in os.environ:
+        app()
+    else:
+        print("Please run 's3-loader auth' before attemting access your bucket.")
